f245b2a204ff2f2fe3d41f61392c8e3640b71ed7
AlexMorrow239 <alex.morrow239@gmail.com>
2025-02-02 16:01:31 -0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ClientQuestionnaire from "./ClientQuestionairre";
+import ClientQuestionnaireLinear from "./Linear";
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="container">
+        <ClientQuestionnaireLinear />
+      </div>
+    </div>
+  );
+}
+
+export default App;
